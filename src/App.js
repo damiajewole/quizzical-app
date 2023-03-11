8122f7c65fb3548ac89297029645ced4b9dfaa62
Ajewole Damilola <damilola4god@gmail.com>
2023-03-11 20:48:41 +0100

    Updates made, component memoized

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { nanoid } from 'nanoid'
 function App(){
   const [show, setShow] = useState(false)
   const [quizdata, setQuizData] = useState([])
+  const [click, setClick] = useState(false)
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple")
@@ -19,34 +20,15 @@ function App(){
     setShow(!show)
   }
 
-<<<<<<< Updated upstream
-  function calculateResult(childData){
-    console.log("results calculated")
-    console.log(childData)
-  }
-=======
   function setRender(){
     setClick(true)
-    // console.log()
   }
 
   console.log(click, "click")
->>>>>>> Stashed changes
 
   return (
     <div>
       {show ? 
-<<<<<<< Updated upstream
-      <div className="quiz page">
-        {quizdata.results.map((data) =>
-            <Questions data={data} key ={nanoid()} handleCallback={calculateResult}/>
-        )}
-        <button className="btn ans" >Check answers</button>
-      </div>
-      : 
-      <Start onClick={renderthis} />} 
-      
-=======
         <div className="quiz page">
           {quizdata.results.map((data) =>
               <Questions data={data} handleClick={click}/>
@@ -63,8 +45,8 @@ function App(){
         : 
         <Start onClick={renderthis} />
       }
->>>>>>> Stashed changes
     </div>
-  )    
+  )
+
 }
 export default App;
--- src/Questions.js
+++ src/Questions.js
@@ -2,31 +2,22 @@ import { nanoid } from 'nanoid';
 import React from 'react';
 import shuffle from "./shuffle";
 import './style.css';
-<<<<<<< Updated upstream
-import { useMemo, useState } from 'react';
-=======
 import { useMemo, useState, memo } from 'react';
->>>>>>> Stashed changes
 
 const MemoizedQuestions = memo(({data, click}) => {
-    // console.log(data.correct_answer)
     const [options, setOptions] = useState([
         ...data.incorrect_answers,
         data.correct_answer
     ])
     const [userAnswer, setUserAnswer] = useState([])
     console.log("click in Questions" ,click)
-    // const [selectedAnswers, setselectedAnswers] = useState([])
     
     // take the array of options and shuffle them, then useMemo to make sure they are only shuffled once, and that is the beginning of the page
     useMemo(() => {
         setOptions(shuffle(options));
     }, [options]);
 
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
     function chooseOption(event){
         let chosenOption = event.currentTarget.id
         setUserAnswer(prevAnswer => [
@@ -39,15 +30,11 @@ const MemoizedQuestions = memo(({data, click}) => {
             <h1 className='question'>{data.question.replace(/&quot;/g, '"')}</h1>
             <div className="answer">
                 {options.map((opt) =>
-                    <div className={(userAnswer == opt) ? "try yes": "try"} key ={nanoid()}  id={`${opt}`} onClick={chooseOption}>
+                    <div className={(userAnswer == opt) ? "try yes": "try"} key ={nanoid()} id={`${opt}`} onClick={chooseOption}>
                         <p className="answer--value" >{opt.replace(/&quot;/g, '"')} </p>
                     </div>
                 )}
             </div>
-<<<<<<< Updated upstream
-            {props.handleCallback(userAnswer)}
-=======
->>>>>>> Stashed changes
         </div>
     )
 })
